68b1a05fd3db1a79aff3218bb53abd39ccc580c8
Muhammad Fahad <fahad.unity1@gmail.com>
2024-09-14 13:16:52 +0500

    Completed Assignment 1

---
+++ Assignment1.py
@@ -0,0 +1,49 @@
+print("Hello, World!")
+print("Fahad")
+
+
+
+
+print("Fahad Sabir")
+print(1, 2, 3)
+
+
+
+
+print("Hello\nWorld")
+print("Hello\tWorld")
+
+
+
+
+print("apple, banana, cherry")
+print("1-2-3")
+
+
+
+
+print("Hello World")
+print("12")
+
+
+
+
+print('He said, "Hello!"')
+print("This is a backslash: \\")
+
+
+
+
+age = 29
+print(f"I am {age} years old")
+number = 3
+print(f"The number is {number}")
+
+
+
+
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
