d613bb40c58ee5fa2d4917aac91333aee6cb3e9d
Muhammad Fahad <fahad.unity1@gmail.com>
2024-09-21 14:17:29 +0500

    Assignment2

---
+++ Assignment2.py
@@ -0,0 +1,110 @@
+message = "Hello, World!"
+print(message)
+
+######################
+
+message = "Hello, World!"
+print(message)
+message = "Change, World!"
+print(message)
+
+######################
+
+person_name = "Eric"
+print(f'Hello {person_name}, would you like to learn some Python today?')
+
+######################
+
+person_name = "Fahad"
+print(person_name.lower())
+print(person_name.upper())
+print(person_name.title())
+
+######################
+
+author = "Maya Angelou"
+quote = "You will face many defeats in life, but never let yourself be defeated."
+print(f'{author} once said, "{quote}"')
+
+######################
+
+name = "\tMuhammad Fahad\n"
+print(name)  # Original with whitespaces
+print(name.lstrip())  # Leading spaces removed
+print(name.rstrip())  # Trailing spaces removed
+print(name.strip())   # Both leading and trailing spaces removed
+
+######################
+
+x = 5
+y = 10
+z = 15
+print(x + y + z)
+
+######################
+
+a = 3
+b = 7
+print(f"Before swap: a = {a}, b = {b}")
+# Swapping values
+a, b = b, a
+print(f"After swap: a = {a}, b = {b}")
+
+######################
+
+color = "Red"
+print(color)
+favorite = color
+print(favorite)
+
+######################
+
+pet_name = "Skooby"
+pet_name = "Doo"
+print(pet_name)
+
+######################
+
+sunshine = "Sunshine"
+print(sunshine) # I see the error
+
+######################
+
+score = 100
+print(score)
+score = 300
+print(score)
+
+######################
+
+city = "Lahore"
+print(city)
+
+######################
+
+text = "python programming"
+print(text.title())
+
+######################
+
+text = "MuhAmmAd FahAd"
+print(text.lower())
+
+######################
+
+text = "MuhAmmAd FahAd"
+print(text.upper())
+
+######################
+
+temperature = 25
+print(f"The current temperature is {temperature} degrees.")
+
+######################
+
+poem = """The sun sets low,
+The stars begin to glow,
+Whispers in the breeze,
+Through the swaying trees."""
+print(poem)
+
