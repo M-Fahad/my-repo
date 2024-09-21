46ba7ff8dbb1b635c77e545c11643ce670e30370
Muhammad Fahad <fahad.unity1@gmail.com>
2024-09-21 14:34:59 +0500

    Assignment4

---
+++ Assignment4.py
@@ -0,0 +1,19 @@
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operation = input("Enter operation (+, -, *, /): ")
+
+# Performing the operation based on user input
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+else:
+    result = "Invalid operation!"
+
+# Printing the result
+print(f"Result: {result}")
